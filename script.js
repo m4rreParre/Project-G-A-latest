5051819ad65b15f10e7689a95311e4ec12df5217
Marre <marrebjoredal@gmail.com>
2024-12-16 11:18:04 +0100

    FixedBiggerSmallerAalement

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function showSidebar(){
     const sidebar = document.querySelector('.sidebar');
     const overlay = document.querySelector('.overlay');
     const closer = document.querySelector('.menu-closer');
+    const links = document.querySelectorAll('.sidebar > li > a');
 
-
+    links.forEach(link => {
+        link.style.width = '100%';
+    });
     sidebar.style.left = '0';
     overlay.style.display = 'flex';
     closer.classList.add('rotate');
@@ -12,8 +15,11 @@ function closeSidebar(){
     const sidebar = document.querySelector('.sidebar');
     const overlay = document.querySelector('.overlay');
     const closer = document.querySelector('.menu-closer');
+    const links = document.querySelectorAll('.sidebar > li > a');
 
-
+    links.forEach(link => {
+        link.style.width = 'fit-content';
+    });
     sidebar.style.left = '-250px';
     overlay.style.display = 'none';
     closer.classList.remove('rotate');
